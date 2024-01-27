7a8b6658268ebb7e65f258ad88da8a6bc90d5aff
craig91 <craigdejean1@gmail.com>
2024-01-27 10:17:38 -0500

    added variable practice file to get used to the syntax

---
+++ variables/vars.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	// showing the different ways to declare a basic variable
+	var example = 3
+
+	var example2 int = 3
+
+	var example3 int // <-- uninitialized variable
+	example3 = 3     // <-- now it is intitialized with a value
+
+	var a, b, c = 1, 2, "sample"
+
+	var (
+		d int = 3
+		e int = 4
+		f     = "sample"
+	)
+
+	// create and assign " := "
+	test := 3 // creates the variable and assigns the value, a shorthand for var test int = 3
+	fmt.Println(test)
+
+	fmt.Println(example, example2, example3)
+	fmt.Println(a, b, c)
+	fmt.Println(d, e, f)
+
+}
