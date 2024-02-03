fa181b8e9cb561cd6b09163be1ee29bac82ffd5c
craig91 <craigdejean1@gmail.com>
2024-02-03 18:29:32 -0500

    added sub tests to helloWorld function

--- hello-world-with-testing/hello_test.go
+++ hello-world-with-testing/hello_test.go
@@ -3,9 +3,29 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("chris")
-	want := "Hello, chris"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("chris")
+		want := "Hello, chris"
+		assertCorrectMessage(t, got, want)
 
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
